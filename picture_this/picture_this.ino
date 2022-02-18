3ea409eede09c4d244d71438ee1e134151bc7ecc
Alex Eastman <alexeast@buffalo.edu>
2022-02-18 12:46:45 -0500

    Add gitignore file. Added SparkFun qwiic relay libraries

--- picture_this/picture_this.ino
+++ picture_this/picture_this.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include "SparkFun_Qwiic_Relay.h"
 
+const int RELAY_ADDR = 0x6D;
+Qwiic_Relay qr(RELAY_ADDR);
+
+void setup()
+{
+  Wire.begin();
+  if (!quad_relay.begin()) {
+    Serial.printf("Incompatible\n");
+  } else {
+    Serial.printf("Successful begin()\n");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
 }
