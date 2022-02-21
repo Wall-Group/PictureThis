bc0ec6f910e930ff70dc9d2a67f893b2851df686
Alex Eastman <alexeast@buffalo.edu>
2022-02-21 12:42:25 -0500

    Added to .gitignore. Added to picture_this.ino. Added Qwiic relay library files

--- picture_this/picture_this.ino
+++ picture_this/picture_this.ino
@@ -1,13 +1,21 @@
 #include <Wire.h>
 #include "SparkFun_Qwiic_Relay.h"
 
+#include <MatrixHardware_Teensy4_ShieldV5.h>
+#include <SmartMatrix.h>
+
+#define COLOR_DEPTH 24
+const uint16_t kMatrixWidth = 32;
+const uint16_t kMatrixHeight = 32;
+const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;
+
 const int RELAY_ADDR = 0x6D;
 Qwiic_Relay qr(RELAY_ADDR);
 
 void setup()
 {
   Wire.begin();
-  if (!quad_relay.begin()) {
+  if (!qr.begin()) {
     Serial.printf("Incompatible\n");
   } else {
     Serial.printf("Successful begin()\n");
