ed61dd23315fd0fde4abf73f3f64b7aa9f6a96e3
Alex Eastman <alexeast@buffalo.edu>
2022-02-25 20:11:54 -0500

    Commit

--- picture_this/picture_this.ino
+++ picture_this/picture_this.ino
@@ -5,10 +5,21 @@
 #include <MatrixHardware_Teensy4_ShieldV5.h>
 #include <SmartMatrix.h>
 
-#define COLOR_DEPTH 24
-const uint16_t kMatrixWidth = 32;
-const uint16_t kMatrixHeight = 32;
-const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;
+#define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
+const uint16_t kMatrixWidth = 64;        // known working: 32, 64, 96, 128
+const uint16_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64
+const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
+const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
+const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
+const uint32_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
+const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
+const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
+const uint8_t kIndexedLayerOptions = (SM_INDEXED_OPTIONS_NONE);
+
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
+SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kIndexedLayerOptions);
 
 const int RELAY_ADDR = 0x6D;
 Qwiic_Relay qr(RELAY_ADDR);
@@ -16,10 +27,12 @@ Qwiic_Relay qr(RELAY_ADDR);
 void setup()
 {
   Wire.begin();
+    matrix.addLayer(&backgroundLayer); 
+  matrix.addLayer(&scrollingLayer); 
+  matrix.addLayer(&indexedLayer); 
+  matrix.begin();
 
   Serial.begin(57600);
-  while(!Serial);
-  Serial.println("Joystick test:");
   
   if (!qr.begin()) {
     Serial.printf("Incompatible\n");
@@ -27,10 +40,15 @@ void setup()
     Serial.printf("Successful begin()\n");
   }
 
-  qr.turnRelayOn(1);
+  // qr.turnRelayOn(1);
 }
 
 void loop() {
-  
+
+  backgroundLayer.fillScreen({0xff,0,0});
+  backgroundLayer.swapBuffers(false);
+  delay(1000);
+  backgroundLayer.fillScreen({0,0xff,0});
+  backgroundLayer.swapBuffers(false);
 
 }
